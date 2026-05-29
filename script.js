83a0dcd15bb07eacb2e489ee2c64d2e587348bde
adjadomariam06 <adjadomariam0@gmail.com>
2026-05-29 18:33:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,102 @@
 const resultat = document.getElementById("resultat");
 const scoreElement = document.getElementById("score");
+const manchesElement = document.getElementById("manches");
+const joueurChoix =
+document.getElementById("joueurChoix");
+const ordiChoix =
+document.getElementById("ordiChoix");
 
 let score = 0;
 
+let manches = 0;
+
 function jouer(choixJoueur) {
 
-    const choixOrdinateur = choixAleatoire();
+    manches++;
+
+    manchesElement.textContent = manches;
+
+    const choixOrdinateur =
+    choixAleatoire();
+
+    joueurChoix.textContent =
+    choixJoueur;
+
+    ordiChoix.textContent =
+    choixOrdinateur;
 
     if (choixJoueur === choixOrdinateur) {
 
         resultat.textContent =
-        "Égalité ! L'ordinateur a choisi " + choixOrdinateur;
+        "Égalité 🤝";
 
+        resultat.style.color = "white";
     }
 
     else if (
 
-        (choixJoueur === "pierre" && choixOrdinateur === "ciseaux") ||
+        (choixJoueur === "pierre" &&
+        choixOrdinateur === "ciseaux")
 
-        (choixJoueur === "papier" && choixOrdinateur === "pierre") ||
+        ||
 
-        (choixJoueur === "ciseaux" && choixOrdinateur === "papier")
+        (choixJoueur === "papier" &&
+        choixOrdinateur === "pierre")
+
+        ||
+
+        (choixJoueur === "ciseaux" &&
+        choixOrdinateur === "papier")
 
     ) {
 
         score++;
 
         resultat.textContent =
-        "Tu as gagné ! L'ordinateur a choisi " + choixOrdinateur;
+        "Tu as gagné 🎉";
 
+        resultat.style.color =
+        "lightgreen";
     }
 
     else {
 
         resultat.textContent =
-        "Tu as perdu ! L'ordinateur a choisi " + choixOrdinateur;
+        "Tu as perdu 😢";
 
+        resultat.style.color =
+        "red";
     }
 
-    scoreElement.textContent = "Score : " + score;
+    scoreElement.textContent = score;
+
+    resultat.style.animation = "none";
+
+    setTimeout(() => {
+
+        resultat.style.animation =
+        "pulse 0.5s";
+
+    }, 10);
 }
 
 function choixAleatoire() {
 
-    const choix = ["pierre", "papier", "ciseaux"];
+    const choix = [
+
+        "pierre",
+
+        "papier",
+
+        "ciseaux"
+    ];
 
     const nombreAleatoire =
     Math.floor(Math.random() * 3);
 
     return choix[nombreAleatoire];
 }
+
+
+       
+   
